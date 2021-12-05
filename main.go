18c9414a6fabe45178075e6f912e02ae39cfed9f
ltbatista <ltbatista30@gmail.com>
2021-12-05 20:06:18 -0300

    add writeToFile function

--- main.go
+++ main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	simplepb "github.com/ltbatista/protobuf-with-go/src/simple"
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
-	doSimple()
+	sm := doSimple()
+
+	writeToFile("simple.bin", sm)
+	//TODO: readFromFile()
 }
 
-func doSimple() {
+func writeToFile(fname string, pb proto.Message) error {
+	out, err := proto.Marshal(pb)
+	if err != nil {
+		log.Fatalln("Can't serialize to bytes ", err)
+		return err
+	}
+
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Can't write to file ", err)
+		return err
+	}
+	fmt.Println("Data has been written.")
+	return nil
+}
+
+func doSimple() *simplepb.SimpleMessage {
 	sm := simplepb.SimpleMessage{
 		Id:         12345,
 		IsSimple:   true,
@@ -25,4 +46,6 @@ func doSimple() {
 	fmt.Println(sm)
 
 	fmt.Println("The ID is: ", sm.GetId())
+
+	return &sm
 }
