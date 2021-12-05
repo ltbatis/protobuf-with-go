50f2d5f61a0870a6ab92672ab22e6085709a6d54
ltbatista <ltbatista30@gmail.com>
2021-12-05 20:23:00 -0300

    readAndWriteDemo()

--- main.go
+++ main.go
@@ -11,9 +11,14 @@ import (
 
 func main() {
 	sm := doSimple()
+	readAndWriteDemo(sm)
+}
 
+func readAndWriteDemo(sm *simplepb.SimpleMessage) {
 	writeToFile("simple.bin", sm)
-	//TODO: readFromFile()
+	sm2 := &simplepb.SimpleMessage{}
+	readFromFile("simple.bin", sm2)
+	fmt.Println("Read the content:\n", sm2)
 }
 
 func writeToFile(fname string, pb proto.Message) error {
@@ -28,6 +33,21 @@ func writeToFile(fname string, pb proto.Message) error {
 		return err
 	}
 	fmt.Println("Data has been written.")
+	return nil
+}
+
+func readFromFile(fname string, pb proto.Message) error {
+	in, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatalln("Something went wrong when reading the file ", err)
+		return err
+	}
+	err2 := proto.Unmarshal(in, pb)
+	if err != nil {
+		log.Fatalln("Couldn't put the bytes into the protocol buffer ", err)
+		return err2
+	}
+
 	return nil
 }
 
