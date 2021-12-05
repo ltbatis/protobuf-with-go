72506a5c99da6460fcfe03cc5bbdc2259922ff46
ltbatista <ltbatista30@gmail.com>
2021-12-05 16:23:57 -0300

    primeiros passos com uma mensagem simples

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	example_simple "github.com/ltbatista/protobuf-with-go/src/simple"
+)
 
 func main() {
-	fmt.Println("Hello World.")
+	doSimple()
+}
+
+func doSimple() {
+	sm := example_simple.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "My Simple Message",
+		SampleList: []int32{1, 4, 7, 8},
+	}
+
+	fmt.Println(sm)
+
+	sm.Name = "I renamed you"
+
+	fmt.Println(sm)
+
+	fmt.Println("The ID is: ", sm.GetId())
 }
